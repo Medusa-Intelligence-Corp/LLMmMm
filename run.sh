32d1209a1960b949bb1a55e0e900c1dc06a616db
Brad Flaugher <brad@bradflaugher.com>
2024-04-17 15:30:52 -0400

    DEEMONSS

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#NOTE: If you are running in production remove the -it flag and add a -d flag instead
 sudo docker run -e OPENROUTER_API_KEY=$OPENROUTER_API_KEY -it --rm -p 5000:5000 sommelier
